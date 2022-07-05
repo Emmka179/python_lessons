de0e14a416017f588b5c691313a23c02bcc68ea7
Alex <bboyaleks@inbox.ru>
2022-07-05 13:16:42 +0600

    00_distance completed (fixed)

--- lesson_002/lesson_002/00_distance.py
+++ lesson_002/lesson_002/00_distance.py
@@ -19,14 +19,14 @@ distances = dict()
 moscow = sites['Moscow']
 london = sites['London']
 paris = sites['Paris']
-moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1])**2) ** .5
-moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1])**2) ** .5
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** .5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
 
 distances['Moscow'] = {}
 distances['Moscow']['London'] = moscow_london
 distances['Moscow']['Paris'] = moscow_paris
 
-london_paris = ((paris[0] - london[0]) ** 2 + (paris[1] - london[1])**2) ** .5
+london_paris = ((paris[0] - london[0]) ** 2 + (paris[1] - london[1]) ** 2) ** .5
 
 distances['London'] = {}
 distances['London']['Moscow'] = moscow_london
@@ -39,6 +39,7 @@ distances['Paris']['London'] = london_paris
 pprint(distances)
 
 #1 task completed
+#Делал задание по видеоуроку, так как там Вадим показывал его выполнение и я точь-в-точь сделал, как у него в ролике
 
 
 
