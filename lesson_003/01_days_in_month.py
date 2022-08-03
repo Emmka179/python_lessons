6ecd16917907894ceae2a885806add84c6ceff74
Alex <bboyaleks@inbox.ru>
2022-08-03 15:36:15 +0600

    Сделал, но все через if

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -9,6 +9,34 @@
 # Номер месяца получать от пользователя следующим образом
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
-print('Вы ввели', month)
+if 1 <= month <= 12:
+    print('Вы ввели', month, 'месяц')
+else:
+    print('Вы ввели неверный месяц')
 
-# TODO здесь ваш код
+if month == 1:
+    print(31)
+if month == 2:
+    print(28)
+if month == 3:
+    print(31)
+if month == 4:
+    print(30)
+if month == 5:
+    print(31)
+if month == 6:
+    print(30)
+if month == 7:
+    print(31)
+if month == 8:
+    print(31)
+if month == 9:
+    print(30)
+if month == 10:
+    print(31)
+if month == 11:
+    print(30)
+if month == 12:
+    print(31)
+
+#TODO здесь ваш код
