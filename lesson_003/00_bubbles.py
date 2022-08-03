b18abed251e70369dcc9b5dfba60c2fc0e41c043
Alex <bboyaleks@inbox.ru>
2022-08-03 13:28:40 +0600

    bubbles completed

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -1,23 +1,61 @@
 # -*- coding: utf-8 -*-
+import random
 
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+# point = sd.get_point(600, 300)
+# sd.circle(center_position=point)
+# radius = 50
+# for _ in range(3):
+#     radius += 5
+#     sd.circle(center_position=point, radius=radius, width=2)
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+# def bubble(point, step):
+#     radius = 50
+#     for _ in range(3):
+#         radius += step
+#         sd.circle(center_position=point, radius=radius, width=2)
+#
+# point = sd.get_point(600, 300)
+# bubble(point=point, step=10)
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+# def bubble(point, step):
+#     radius = 50
+#     for _ in range(1):
+#         radius += step
+#         sd.circle(center_position=point, radius=radius, width=2)
+# for x in range(150, 1101, 100):
+#     point = sd.get_point(x, 300)
+#     bubble(point=point, step=10)
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+# def bubble(point, step):
+#     radius = 50
+#     for _ in range(1):
+#         radius += step
+#         sd.circle(center_position=point, radius=radius, width=2)
+# for y in range(200, 401, 100):
+#     for x in range(150, 1101, 100):
+#         point = sd.get_point(x, y)
+#         bubble(point=point, step=10)
+
+
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+# def bubble(point, step):
+#     radius = 50
+#     for _ in range(1):
+#         radius += step
+#         sd.circle(center_position=point, radius=radius, width=2)
+# for _ in range(100):
+#     point = sd.random_point()
+#     step = random.randint(2, 10)
+#     bubble(point=point, step=step)
 
 sd.pause()
 
